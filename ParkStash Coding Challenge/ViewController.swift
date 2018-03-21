29b242f83c5c2511f986e06e32c688bed7c8a211
Shreyas Zagade <shreyaszagade@gmail.com>
2018-03-21 15:09:32 -0400

    Initial commit with gitignore

---
+++ ParkStash Coding Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkStash Coding Challenge
+//
+//  Created by Shreyas Zagade on 3/21/18.
+//  Copyright © 2018 Shreyas Zagade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
